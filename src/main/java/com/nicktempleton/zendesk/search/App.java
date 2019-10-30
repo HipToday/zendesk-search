85868ee95484d1b27dbbe5e8655b2116f4f83cb2
Nick Templeton <>
2019-10-30 09:36:31 -0500

    Still very ugly, but basic search works

--- src/main/java/com/nicktempleton/zendesk/search/App.java
+++ src/main/java/com/nicktempleton/zendesk/search/App.java
@@ -21,14 +21,13 @@
  */
 package com.nicktempleton.zendesk.search;
 
+import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Scanner;
 import java.util.Set;
-import java.util.stream.Collectors;
 
-import com.nicktempleton.zendesk.search.function.Predicates;
 import com.nicktempleton.zendesk.search.util.ResourceLoader;
 
 /**
@@ -75,19 +74,43 @@ public class App {
         }
     }
 
+    @SuppressWarnings("unchecked")
     private static void search(List<Map<String, Object>> data, Scanner scanner) {
         String searchField = promptForSearchableField(data, scanner);
         if (data.get(0).keySet().contains(searchField)) {
             String searchValue = promptForSearchValue(scanner);
-            List<Map<String, Object>> result = data.stream()
-                .filter(org -> null != org.get(searchField))
-                .filter(Predicates.isDouble(searchField)
-                    .and(org -> org.get(searchField).equals(Double.valueOf(searchValue)))
-                    .or(Predicates.isDouble(searchField).negate()
-                        .and(org -> org.get(searchField).toString().contains(searchValue))))
-//                .filter(org -> org.get(searchField).toString().contains(searchValue))
-                .collect(Collectors.toList());
-            result.forEach(System.out::println);
+            List<Map<String, Object>> results = new ArrayList<>();
+            for (Map<String, Object> listItem : data) {
+                Object fieldValue = listItem.get(searchField);
+                System.out.println(null != fieldValue ? fieldValue.getClass() : "null");
+                if (fieldValue instanceof Double) {
+                    try {
+                        if (fieldValue.equals(Double.valueOf(searchValue))) {
+                            results.add(listItem);
+                        }
+                    } catch (NumberFormatException nfe) {
+                        // move on
+                    }
+                } else if (fieldValue instanceof ArrayList) {
+                    if (((ArrayList<String>)fieldValue).contains(searchValue)) {
+                        results.add(listItem);
+                    }
+                } else if (fieldValue instanceof Object) {
+                    if (fieldValue.toString().equals(searchValue)) {
+                        results.add(listItem);
+                    }
+                }
+            }
+            results.forEach(System.out::println);
+//             List<Map<String, Object>> result = data.stream()
+//                 .filter(org -> null != org.get(searchField))
+//                 .filter(Predicates.isDouble(searchField)
+//                     .and(org -> org.get(searchField).equals(Double.valueOf(searchValue)))
+//                     .or(Predicates.isDouble(searchField).negate()
+//                         .and(org -> org.get(searchField).toString().contains(searchValue))))
+// //                .filter(org -> org.get(searchField).toString().contains(searchValue))
+//                 .collect(Collectors.toList());
+//             result.forEach(System.out::println);
         }
     }
 
