4ac38359cb6cb6c6e0767ff3a8e7d7c59741e5bb
Nick Templeton <>
2019-10-30 13:30:20 -0500

    We don't care about naming policy anymore

--- src/main/java/com/nicktempleton/zendesk/search/util/ResourceLoader.java
+++ src/main/java/com/nicktempleton/zendesk/search/util/ResourceLoader.java
@@ -29,9 +29,7 @@ import java.lang.reflect.Type;
 import java.util.List;
 import java.util.Map;
 
-import com.google.gson.FieldNamingPolicy;
 import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
 import com.google.gson.JsonSyntaxException;
 import com.google.gson.reflect.TypeToken;
 
