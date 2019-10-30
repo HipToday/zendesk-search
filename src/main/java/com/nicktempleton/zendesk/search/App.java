131ef5b3fe78b5636aa75fec09c48850878128f7
Nick Templeton <>
2019-10-30 00:23:10 -0500

    Ugly, but sorta working search.

--- src/main/java/com/nicktempleton/zendesk/search/App.java
+++ src/main/java/com/nicktempleton/zendesk/search/App.java
@@ -21,11 +21,14 @@
  */
 package com.nicktempleton.zendesk.search;
 
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Scanner;
+import java.util.Set;
 import java.util.stream.Collectors;
 
+import com.nicktempleton.zendesk.search.function.Predicates;
 import com.nicktempleton.zendesk.search.util.ResourceLoader;
 
 /**
@@ -50,5 +53,60 @@ public class App {
         System.out.println("Organization count: " + organizations.size());
         System.out.println("Ticket count: " + tickets.size());
         System.out.println("User count: " + users.size());
+
+        try (Scanner scanner = new Scanner(System.in)) {
+            String input;
+            do {
+                System.out.println("Please select an option:");
+                System.out.println("1 - Search Organizations");
+                System.out.println("2 - Search Tickets");
+                System.out.println("3 - Search Users");
+                System.out.println("4 - Quit");
+                System.out.print("> ");
+                input = scanner.next();
+                if ("1".equals(input)) {
+                    search(organizations, scanner);
+                } else if ("2".equals(input)) {
+                    search(tickets, scanner);
+                } else if ("3".equals(input)) {
+                    search(users, scanner);
+                }
+            } while (!"4".equals(input));
+        }
+    }
+
+    private static void search(List<Map<String, Object>> data, Scanner scanner) {
+        String searchField = promptForSearchableField(data, scanner);
+        if (data.get(0).keySet().contains(searchField)) {
+            String searchValue = promptForSearchValue(scanner);
+            List<Map<String, Object>> result = data.stream()
+                .filter(org -> null != org.get(searchField))
+                .filter(Predicates.isDouble(searchField)
+                    .and(org -> org.get(searchField).equals(Double.valueOf(searchValue)))
+                    .or(Predicates.isDouble(searchField).negate()
+                        .and(org -> org.get(searchField).toString().contains(searchValue))))
+//                .filter(org -> org.get(searchField).toString().contains(searchValue))
+                .collect(Collectors.toList());
+            result.forEach(System.out::println);
+        }
+    }
+
+    private static String promptForSearchableField(List<Map<String, Object>> data, Scanner scanner) {
+        Set<String> searchableFields = new HashSet<>();
+        for (Map<String, Object> org : data) {
+            searchableFields.addAll(org.keySet());
+        }
+
+        System.out.println("Searchable fields:");
+        searchableFields.forEach(System.out::println);
+        System.out.println("Enter desired field to search:");
+        System.out.print("> ");
+        return scanner.next();
+    }
+
+    private static String promptForSearchValue(Scanner scanner) {
+        System.out.println("Enter search value:");
+        System.out.print("> ");
+        return scanner.next();
     }
 }
