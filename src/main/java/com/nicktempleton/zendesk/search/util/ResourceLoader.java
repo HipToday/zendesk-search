4262a847c31c3be5cfc25a02b34981576b0173c6
Nick Templeton <>
2019-10-29 23:26:32 -0500

    Unused import

--- src/main/java/com/nicktempleton/zendesk/search/util/ResourceLoader.java
+++ src/main/java/com/nicktempleton/zendesk/search/util/ResourceLoader.java
@@ -26,7 +26,6 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.lang.reflect.Type;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
